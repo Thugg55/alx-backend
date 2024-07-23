d1ab3bbad6183cd33fcea60a8387dc26c4bde12f
Thugg55 <thuggeru7@gmail.com>
2024-07-22 20:54:26 -0700

    irst task

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Defines a function named `index_range`
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Args:
+        page (int): the current page
+        page_size (int): the amount of items in a page
+    Returns:
+        (tuple): a tuple of the start and end index for the given page
+    """
+    nextPageS = page * page_size
+    return nextPageS - page_size, nextPageS
